12db2ad435dd344efeebe97762c09612557b8f21
pakito072 <105859870+pakito072@users.noreply.github.com>
2024-11-05 10:54:25 +0100

    delete0.1

--- procesar.php
+++ procesar.php
@@ -37,10 +37,33 @@ function get($table){
 
 }
 
+function delete ($table, $id){
+  
+  $conn = conectarDB();
 
+  $stmt = $conn->prepare("DELETE FROM " . $table . " WHERE id = ?");
+  $stmt->bind_param("i",$id);
+
+  if ($stmt->execute()) {
+    http_response_code(200);
+  }else{
+    http_response_code(500);
+  }
+
+  $stmt->close();
+  $conn->close();
+
+}
 
 if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET["table"])) {
   
   get($_GET["table"]);
 
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "DELETE") {
+
+  parse_str(file_get_contents("php://input"),$data);
+  $table = isset
+  
 }
